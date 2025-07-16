0957e86b3c1a4eae3313347cc345d5f2a69e02fd
Mattar17 <mattar17x@gmail.com>
2025-07-16 18:36:22 +0300

    Pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import icons from 'url:../img/icons.svg';
 import * as model from './model.js';
 import RecipeView from './views/recipeView.js';
 import SearchView from './views/searchView.js';
+import PaginationView from './views/paginationView.js'
 // import { getJSON, responseTimeout } from './helpers.js';
 
 
@@ -21,6 +22,7 @@ const showRecipe = async function (e) {
     RecipeView.renderSpinner();
     await model.loadRecipe(id);
     RecipeView.render(model.state.recipe);
+    PaginationView.render(model.state.search);
   }
   catch (err) {
     recipeView.renderError(err);
@@ -31,14 +33,22 @@ const showRecipe = async function (e) {
 const searchRecipe = async function (e) {
   e.preventDefault();
   try {
+
+    model.state.search.query = SearchView.getQuery();
     SearchView.renderSpinner();
-    await model.loadSearchResults(SearchView.getQuery());
-    SearchView.render(model.state.search);
+    await model.loadSearchResults();
+    SearchView.render(model.state.search.results);
+    SearchView.render(model.getSearchResultPage());
+    PaginationView.render(model.state.search);
 
   } catch (err) {
     SearchView.renderError(err);
   }
 
+}
+
+const contorlPagination = async function () {
+  PaginationView.render(model.state.search);
 }
 
 const init = function () {
